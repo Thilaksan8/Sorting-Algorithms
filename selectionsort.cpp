f74c89a8ee788e1280f2f5dfe3974de315dc1e33
Thilaksan8 <thilakshanlat@gmail.com>
2025-07-19 10:20:59 +0530

    created selection

---
+++ selectionsort.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void selection(){
+
+}
+void printarray(vector<int>&arr){
+    cout<<"Sorted elements";
+    for(int val:arr)
+        cout<<val<<" ";
+}
+int main(){
+    vector<int>arr;
+    int input;
+    cout<<"Enter elements(Press Ctrl+D or Ctrl+z when done)";
+    while(cin>>input)
+        arr.push_back(input);
+    
+    return 0;
+}
